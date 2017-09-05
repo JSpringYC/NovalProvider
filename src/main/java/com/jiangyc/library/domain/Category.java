2153809db400ed22c3518957ebe0555622306670
姜永春 <710183940@qq.com>
2017-09-05 09:36:39 +0800

    更新实体类

---
+++ src/main/java/com/jiangyc/library/domain/Category.java
@@ -0,0 +1,14 @@
+package com.jiangyc.library.domain;
+
+/**
+ * 小说的分类
+ */
+public class Category {
+    /** 分类编号 */
+    private String id;
+    /** 分类名称 */
+    private String name;
+    /** 父分类 */
+    private Category parent;
+
+}
---
+++ src/main/java/com/jiangyc/library/domain/Noval.java
@@ -0,0 +1,16 @@
+package com.jiangyc.library.domain;
+
+/**
+ * 小说模型
+ */
+public class Noval {
+    /** 小说编号 */
+    private String id;
+    /** 小说名称 */
+    private String name;
+    /** 小说作者 */
+    private String author;
+    /** 小说分类 */
+    private Category category;
+    
+}
